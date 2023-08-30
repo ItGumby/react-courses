c1758989c0669e498db64b994c1909bf8f414464
Brian Street <brian.c.street@gmail.com>
2023-08-29 20:00:20 -0600

    enhance <Articles /> to CSR load article data

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Articles = () => {
-    return (
-        <h1>Articles</h1>
+  const [articles, setArticles] = useState();
+
+  // initial load of data from "api"
+  useEffect(() => {
+    fetch('/api/articles')
+      .then( response => response.json())
+      .then( data => setArticles(data))
+      .catch( err => { console.error("data fetch failed..."); });
+    console.debug(`loaded {articles.length} articles`);
+  }, []);
+
+  return (
+    <>
+      <h1>Articles</h1>
+      <ul>
+      { articles && articles.map(article => (
+        <li key={article.title}>
+          <h3>{article.title}</h3>
+          <p>by {article.author}</p>
+        </li>
+      ))}
+      </ul>
+    </>
     );
 }
