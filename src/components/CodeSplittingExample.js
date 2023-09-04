10f5809eb27dfe08579c6e0c311d3581328d140e
Brian Street <brian.c.street@gmail.com>
2023-09-04 11:14:55 -0600

    code-splitting with lazy-load from component

---
+++ src/components/CodeSplittingExample.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CodeSplittingExample =() => {
+  return (
+    <p>this component will be separated from the app</p>
+  );
+}
+
+export default CodeSplittingExample;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+
+// traditional "big ball of JS" import
+// import CodeSplittingExample from '../components/CodeSplittingExample';
+// alternate code-splittting via lazy load of module
+const CodeSplittingExample = lazy(() => import('../components/CodeSplittingExample'));
 
 export const About = () => {
-    return (
-        <h1>About</h1>
-    );
+  return (
+    <>
+      <h1>About</h1>
+      <Suspense fallback={<p>loading...</p>}>
+        <CodeSplittingExample />
+      </Suspense>
+    </>
+  );
 }
